a6c828cbd9087f336b41e8a5005a52b26cdf8a3a
r.smoter <r.smoter@172.28.4.75>
2016-02-10 08:01:11 +0100

    test

---
+++ src/helloworld/Klasa1.java
@@ -0,0 +1,28 @@
+package helloworld;
+
+import java.util.Random;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author r.smoter
+ */
+public class Klasa1 {
+
+public void  Print(){
+    
+    
+    Random rnd = new Random();
+    
+    rnd.nextInt(10);
+    System.out.println( rnd );
+    
+    
+}
+    
+}
